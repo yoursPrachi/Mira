cdd0d82f83def48303c02c84346b22ee27cae008
yoursPrachi <yogeshbalajiearth@gmail.com>
2025-12-19 02:05:22 +0530

    Fix export statement for Firestore database

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Firebase Initialization
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
